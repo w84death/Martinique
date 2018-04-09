a39b0f51331e644c4e752bad040eaa4ccc5797dd
Krzysztof Jankowski <w84death@gmail.com>
2018-04-09 23:59:24 +0200

    oh oh oh ohahoy version

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,37 @@
 extends KinematicBody
 
 var trusters = Vector3(0,0,0)
+var speed = 24
+var MOUSESPEED = 0.005
 
 func _ready():
-
-	pass
-
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	set_process_input(true)
+	
 func _process(delta):
 
-	var thumb_x = Input.get_joy_axis(0, 0) * -1
-	var thumb_y = Input.get_joy_axis(0, 1) * -1
-	var thumb2_x = Input.get_joy_axis(0, 2)
-	var thumb2_y = Input.get_joy_axis(0, 3)
-	
-	trusters = Vector3(thumb2_x, thumb_y, thumb2_y)
-	self.move_and_collide(trusters)
+	if Input.is_key_pressed(KEY_W):
+		translate(Vector3(0, 0, speed*delta))
+	if Input.is_key_pressed(KEY_S):
+		translate(Vector3(0, 0, -speed*delta))
+	if Input.is_key_pressed(KEY_A):
+		translate(Vector3(speed*delta, 0, 0))
+	if Input.is_key_pressed(KEY_D):
+		translate(Vector3(-speed*delta, 0, 0))
 
+	if Input.is_key_pressed(KEY_E):
+		translate(Vector3(0, speed*delta, 0))
+	if Input.is_key_pressed(KEY_Q):
+		translate(Vector3(0, -speed*delta, 0))
+		
 func activate_player():
 	get_node("eyes").make_current()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x*MOUSESPEED
+		rotation.x = clamp(rotation.x + event.relative.y*MOUSESPEED, deg2rad(-90), deg2rad(90))
+
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
